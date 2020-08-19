e26147776e93510403fae67a9286bba6babfd035
tomsuad <tomsuad7@gmail.com>
2020-08-19 12:28:05 +0300

    commitfrom kali

--- make_ap/evil-twin.py
+++ make_ap/evil-twin.py
@@ -2,6 +2,7 @@ import os
 
 print("hi, do you on my  evil-twin code, enjoy")
 linuxOS = input("for kali press 1, for ubuntu press 2: \n")
+os.system('ifconfig')
 interface = input("pls enter your interface name: \n")
 ssid=input(" pls enter your ssid name: \n")
 line="python3 fake_ap.py " + linuxOS +" " + interface + " " + ssid
