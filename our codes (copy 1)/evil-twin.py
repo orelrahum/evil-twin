75908ddacf70fb997f0c22b9312586aec83bee5f
orelrahum <orel.rahum@gmail.com>
2020-08-19 05:16:53 -0400

    commitfrom kali

--- our codes (copy 1)/evil-twin.py
+++ our codes (copy 1)/evil-twin.py
@@ -4,5 +4,5 @@ print("hi, do you on my  evil-twin code, enjoy")
 linuxOS = input("for kali press 1, for ubuntu press 2: \n")
 interface = input("pls enter your interface name: \n")
 ssid=input(" pls enter your ssid name: \n")
-line="python3 fake_ap_start.py " + linuxOS +" " + interface + " " + ssid
+line="python3 fake_ap.py " + linuxOS +" " + interface + " " + ssid
 os.system(line)
