d56a2338b5a2c2f88ce0c7f2ce7e2e062481d23e
orelrahum <orelrahum@Orel-Kali.Home>
2020-09-07 12:48:21 +0300

    commit from kali

---
+++ readme.py
@@ -0,0 +1,20 @@
+#get from github our project
+
+git clone https://"username":"pass"@github.com/orelrahum/evil-twin
+
+#regular update
+
+sudo apt-get update
+sudo apt-get upgrade
+
+
+#install aptche2
+
+sudo apt install apache2
+
+#change exicting folder htmp to var/www
+mv /var/www/html var/www/html_old
+
+#copy html folder from our code
+
+
