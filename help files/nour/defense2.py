aad0bc74fee1b5f3ff6026d96b1ba21695e5d186
orelrahum <orel.rahum@gmail.com>
2020-09-08 11:08:28 -0400

    commit from kali

--- help files/nour/defense2.py
+++ help files/nour/defense2.py
@@ -9,9 +9,9 @@ def PacketHandler(pkt):
     global count
 #    print "got pkt"
     #if pkt.haslayer(Dot11FCS):
-    if pkt.type == 0 and pkt.subtype == 0xc:
-	count = count + 1
-        print "Deauth packet sniffed: %d" % (count)
+     if pkt.type == 0 and pkt.subtype == 0xc:
+	 count = count + 1
+         print "Deauth packet sniffed: %d" % (count)
     if count >= 20:
         print "warning you'r AP is under deauthattack -- could lead to Evil_Twin attack or to Rouge AP call the police for more info"
 
